a85562e265decfc036290e8a50d77bacfc95cf00
ErikJava <112395602+ErikJava@users.noreply.github.com>
2023-12-03 22:34:00 +0100

    final version

---
+++ src/Miniprojekt2/main/Prime.java
@@ -0,0 +1,78 @@
+package Miniprojekt2.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This program counts the sum and the number of prime (int) numbers in interval 0-1000
+ */
+public class Prime {
+
+    private final List<Integer> primes;
+    private final int firstNumber;
+    private final int lastNumber;
+    private int count = 0;
+    private int sumOfPrimes = 0;
+
+    public Prime(int firstNumber, int lastNumber) {
+        this.firstNumber = firstNumber;
+        this.lastNumber = lastNumber;
+
+        if (firstNumber < 0 || lastNumber < 0 || lastNumber > 1000 || firstNumber > lastNumber) {
+            throw new IllegalArgumentException("Hoppsan, fel intervall angivet!");
+        }
+
+        primes = new ArrayList<>();
+        calculatePrimes(firstNumber, lastNumber);
+    }
+
+    private boolean numIsPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number % 2 == 0) {
+            return number == 2;
+        }
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void calculatePrimes(int current, int stop) {
+        if (current > stop) {
+            return;
+        }
+
+        if (numIsPrime(current)) {
+            primes.add(current);
+            count++;
+            sumOfPrimes += current;
+        }
+
+        calculatePrimes(++current, stop);
+    }
+
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getSumOfPrimes() {
+        return sumOfPrimes;
+    }
+
+    public void printCount() {
+        System.out.println("Hej, det finns " + getCount() + " primtal mellan " + firstNumber + " och " + lastNumber + "!");
+    }
+
+    public void printSum() {
+        System.out.println("Och den totala summan av dessa primtal är " + getSumOfPrimes());
+    }
+}
---
+++ src/Miniprojekt2/test/PrimeTest.java
@@ -0,0 +1,131 @@
+package Miniprojekt2.test;
+
+
+import Miniprojekt2.main.Prime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeTest {
+
+    @Test
+    @DisplayName("Testing range without primes")
+    void testRangeWithNoPrimes() {
+        Prime prime = new Prime(24, 25);
+        assertTrue(prime.getPrimes().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Testing a single prime")
+    public void testSingleNumberRange() {
+        Prime prime = new Prime(7, 7);
+        assertNotNull(prime.getPrimes());
+        assertEquals(1, prime.getCount());
+        assertEquals(7, prime.getSumOfPrimes());
+    }
+
+
+    @Test
+    @DisplayName("Initializing of the constructor test")
+    void testConstructorInitializer() {
+        Prime prime = new Prime(0, 1000);
+        assertNotNull(prime.getPrimes());
+        assertFalse(prime.getPrimes().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Initializing of the constructor, test for a small valid range")
+    void testPrimeCalculationForSmallRange() {
+        Prime prime = new Prime(0, 10);
+
+        int[] expectedPrimes = {2, 3, 5, 7};
+        for (int expectedPrime : expectedPrimes) {
+            assertTrue(prime.getPrimes().contains(expectedPrime));
+        }
+
+        assertEquals(expectedPrimes.length, prime.getPrimes().size());
+    }
+
+    @Test
+    @DisplayName("getPrimes for valid range test")
+    void testGetPrimes() {
+        Prime prime = new Prime(10, 20);
+        List<Integer> expectedPrimes = Arrays.asList(11, 13, 17, 19);
+        assertEquals(expectedPrimes, prime.getPrimes());
+    }
+
+    @Test
+    @DisplayName("Test for a very high upper limit")
+    void testExtremeUpperLimit() {
+        Prime prime = new Prime(998, 1000);
+        assertTrue(prime.getPrimes().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Test for a very low lower limit")
+    void testExtremeLowerLimit() {
+        Prime prime = new Prime(0, 2);
+        assertEquals(List.of(2), prime.getPrimes());
+    }
+
+    @Test
+    @DisplayName("Initializing of the constructor, test for the boundary values")
+    void testConstructorForBoundaryValues() {
+        Prime primeAtZero = new Prime(0, 0);
+        assertTrue(primeAtZero.getPrimes().isEmpty());
+
+        Prime primeAtMax = new Prime(1000, 1000);
+        assertEquals(0, primeAtMax.getPrimes().size());
+    }
+
+    @Test
+    @DisplayName("getPrimes for valid lower bound range test")
+    void testGetPrimesLowerInterval() {
+        Prime prime = new Prime(0, 100);
+        List<Integer> expectedPrimes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                41, 43,47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
+        assertEquals(expectedPrimes, prime.getPrimes());
+    }
+
+    @Test
+    @DisplayName("getPrimes for valid upper bound range test")
+    void testGetPrimesUpperInterval() {
+        Prime prime = new Prime(900, 1000);
+        List<Integer> expectedPrimes = Arrays.asList(907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997);
+        assertEquals(expectedPrimes, prime.getPrimes());
+    }
+
+    @Test
+    @DisplayName("Count primes for a known range test")
+    void testCountOfPrimes() {
+        Prime prime = new Prime(0, 10);
+        assertEquals(4, prime.getCount());
+    }
+
+    @Test
+    @DisplayName("The sum of primes for known range test")
+    void testSumOfPrimes() {
+        Prime prime = new Prime(0, 10);
+        int expectedSum = 2 + 3 + 5 + 7;
+        assertEquals(expectedSum, prime.getSumOfPrimes());
+    }
+
+    @Test
+    @DisplayName("Specific exception test with a message for invalid ranges")
+    void testExceptionMessageForInvalidRange() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Prime(-1, 1000));
+        assertEquals("Hoppsan, fel intervall angivet!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Another specific exception test with a message for inverted ranges")
+    void testExceptionMessageForInvertedRange() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Prime(1000, 0));
+        assertEquals("Hoppsan, fel intervall angivet!", exception.getMessage());
+    }
+
+}
